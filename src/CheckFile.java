53f859353dfd9d678ff078ad593d5bc318b48670
SupunRanatunge <supunr.15@cse.mrt.ac.lk>
2020-01-06 00:27:20 +0530

    initial commit

---
+++ src/CheckFile.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class CheckFile {
+
+    // class variable
+    final String lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345674890";
+
+    final java.util.Random rand = new java.util.Random();
+
+    // consider using a Map<String,Boolean> to say whether the identifier is being used or not
+    final Set<String> identifiers = new HashSet<String>();
+
+    public String randomIdentifier() {
+        StringBuilder builder = new StringBuilder();
+        while(builder.toString().length() == 0) {
+            int length = rand.nextInt(5)+5;
+            for(int i = 0; i < length; i++) {
+                builder.append(lexicon.charAt(rand.nextInt(lexicon.length())));
+            }
+            if(identifiers.contains(builder.toString())) {
+                builder = new StringBuilder();
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        CheckFile chkFile = new CheckFile();
+        String name = chkFile.randomIdentifier();
+        System.out.println(name);
+
+    }
+
+
+}
